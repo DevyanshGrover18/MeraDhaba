c8b28e284af4383bbb6689c8dcc31955e3cbbca0
DevyanshGrover18 <devyansh.grover348@gmail.com>
2024-04-04 17:26:02 +0530

    doneeeee
    finally done

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
 async function connectToMongoDB() {
-    await mongoose.connect('mongodb://localhost:27017/MeraDhaba')
+    await mongoose.connect('mongodb+srv://devyanshgrover348:devogrover18@cluster0.0zltntp.mongodb.net/MeraDhabaa?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
     const fetchedData= await mongoose.connection.db.collection("foodList")
     const data = await fetchedData.find({}).toArray()
-
     global.foodItems= data
 }
 
